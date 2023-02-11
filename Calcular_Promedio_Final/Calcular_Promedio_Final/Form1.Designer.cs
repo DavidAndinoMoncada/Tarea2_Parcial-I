b8b414cd0bc0ff5ebfda85b1b769f8c1b1e4184f
DavidAndinoMoncada <dandinom@unah.hn>
2023-02-11 01:47:24 -0600

    Tarea_2
    Funcion Asincrona

---
+++ Calcular_Promedio_Final/Calcular_Promedio_Final/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Calcular_Promedio_Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PrimerParcialtextBox = new System.Windows.Forms.TextBox();
+            this.SegundoParcialtextBox = new System.Windows.Forms.TextBox();
+            this.TercerParcialtextBox = new System.Windows.Forms.TextBox();
+            this.CuartoParcialtextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CalcularPromedioAsyncronobutton = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PrimerParcialtextBox
+            // 
+            this.PrimerParcialtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PrimerParcialtextBox.Location = new System.Drawing.Point(433, 180);
+            this.PrimerParcialtextBox.Name = "PrimerParcialtextBox";
+            this.PrimerParcialtextBox.Size = new System.Drawing.Size(121, 32);
+            this.PrimerParcialtextBox.TabIndex = 0;
+            this.PrimerParcialtextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // SegundoParcialtextBox
+            // 
+            this.SegundoParcialtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SegundoParcialtextBox.Location = new System.Drawing.Point(433, 231);
+            this.SegundoParcialtextBox.Name = "SegundoParcialtextBox";
+            this.SegundoParcialtextBox.Size = new System.Drawing.Size(121, 32);
+            this.SegundoParcialtextBox.TabIndex = 1;
+            this.SegundoParcialtextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TercerParcialtextBox
+            // 
+            this.TercerParcialtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TercerParcialtextBox.Location = new System.Drawing.Point(433, 288);
+            this.TercerParcialtextBox.Name = "TercerParcialtextBox";
+            this.TercerParcialtextBox.Size = new System.Drawing.Size(121, 32);
+            this.TercerParcialtextBox.TabIndex = 2;
+            this.TercerParcialtextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // CuartoParcialtextBox
+            // 
+            this.CuartoParcialtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CuartoParcialtextBox.Location = new System.Drawing.Point(433, 345);
+            this.CuartoParcialtextBox.Name = "CuartoParcialtextBox";
+            this.CuartoParcialtextBox.Size = new System.Drawing.Size(121, 32);
+            this.CuartoParcialtextBox.TabIndex = 3;
+            this.CuartoParcialtextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(238, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Primer Parcial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(238, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Segundo Parcial";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(238, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tercer Parcial";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(238, 349);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Cuarto Parcial";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(281, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(261, 39);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Promedio Final";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(168, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(467, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Acontinuación Ingrese su Nota de Cada Parcial";
+            // 
+            // CalcularPromedioAsyncronobutton
+            // 
+            this.CalcularPromedioAsyncronobutton.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.CalcularPromedioAsyncronobutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalcularPromedioAsyncronobutton.ForeColor = System.Drawing.SystemColors.Window;
+            this.CalcularPromedioAsyncronobutton.Location = new System.Drawing.Point(331, 418);
+            this.CalcularPromedioAsyncronobutton.Name = "CalcularPromedioAsyncronobutton";
+            this.CalcularPromedioAsyncronobutton.Size = new System.Drawing.Size(140, 72);
+            this.CalcularPromedioAsyncronobutton.TabIndex = 10;
+            this.CalcularPromedioAsyncronobutton.Text = "Calcular Promedio";
+            this.CalcularPromedioAsyncronobutton.UseVisualStyleBackColor = false;
+            this.CalcularPromedioAsyncronobutton.Click += new System.EventHandler(this.CalcularPromedioAsyncronobutton_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(822, 560);
+            this.Controls.Add(this.CalcularPromedioAsyncronobutton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CuartoParcialtextBox);
+            this.Controls.Add(this.TercerParcialtextBox);
+            this.Controls.Add(this.SegundoParcialtextBox);
+            this.Controls.Add(this.PrimerParcialtextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PrimerParcialtextBox;
+        private System.Windows.Forms.TextBox SegundoParcialtextBox;
+        private System.Windows.Forms.TextBox TercerParcialtextBox;
+        private System.Windows.Forms.TextBox CuartoParcialtextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button CalcularPromedioAsyncronobutton;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Calcular_Promedio_Final/Calcular_Promedio_Final/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcular_Promedio_Final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void CalcularPromedioAsyncronobutton_Click(object sender, EventArgs e)
+        {
+            if (PrimerParcialtextBox.Text == "")
+            {
+                errorProvider1.SetError(PrimerParcialtextBox, "Ingrese un Valor");
+                errorProvider1.Clear();
+                return;
+
+            }
+            if (SegundoParcialtextBox.Text == "")
+            {
+                errorProvider1.SetError(SegundoParcialtextBox, "Ingrese un Valor");
+                return;
+            }
+            if (TercerParcialtextBox.Text == "")
+            {
+                errorProvider1.SetError(TercerParcialtextBox, "Ingrese un Valor");
+                return;
+            }
+            if (CuartoParcialtextBox.Text == "")
+            {
+                errorProvider1.SetError(CuartoParcialtextBox, "Ingrese un Valor");
+                return;
+            }
+
+            errorProvider1.Clear();
+
+            decimal Parcial1 = Convert.ToDecimal(PrimerParcialtextBox.Text);
+            decimal Parcial2 = Convert.ToDecimal(SegundoParcialtextBox.Text);
+            decimal Parcial3 = Convert.ToDecimal(TercerParcialtextBox.Text);
+            decimal Parcial4 = Convert.ToDecimal(CuartoParcialtextBox.Text);
+
+            decimal Promedio = await CalcularPromedioAsync(Parcial1, Parcial2, Parcial3, Parcial4);
+
+            MessageBox.Show("Tiene un Promedio de: " + Promedio, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        
+        }
+
+        private async Task<decimal> CalcularPromedioAsync(decimal num1, decimal num2, decimal num3, decimal num4)
+        {
+            decimal promedio = await Task.Run(() =>
+            {
+                return (num1 + num2 + num3 + num4) / 4;
+            });
+
+            return promedio;
+        }
+    }
+}
